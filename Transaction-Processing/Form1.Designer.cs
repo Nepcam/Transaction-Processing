c0646ed7db87451b3abc74208ebe8008e4662604
Nepcam <cameron.nepe@gmail.com>
2020-05-26 19:31:51 +1200

    setting up

--- Transaction-Processing/Form1.Designer.cs
+++ Transaction-Processing/Form1.Designer.cs
@@ -28,13 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnProcessFile = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnProcessFile
+            // 
+            this.btnProcessFile.Location = new System.Drawing.Point(12, 186);
+            this.btnProcessFile.Name = "btnProcessFile";
+            this.btnProcessFile.Size = new System.Drawing.Size(169, 78);
+            this.btnProcessFile.TabIndex = 0;
+            this.btnProcessFile.Text = "Process File";
+            this.btnProcessFile.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(259, 186);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(169, 78);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(440, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnProcessFile);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnProcessFile;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Transaction-Processing/Form1.cs
+++ Transaction-Processing/Form1.cs
@@ -4,17 +4,31 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Transaction_Processing
 {
+    // Cameron Nepe - Student ID: 1262199
+
     public partial class Form1 : Form
     {
+        //SET variables
+        string weaponName = "";
+        string weaponType = "";
+        int damageValue = 0;
+        double attackSpeed = 0.0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private double CalculateDPS(int damageValue, double attackSpeed) //DPS == Damage per second
+        {
+            //DPS is calculated by taking the damage value(int) and dividing it by the attack speed(double).
         }
     }
 }
